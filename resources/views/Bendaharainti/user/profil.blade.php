ed12155935f5c88872448a8fa43ce4ab21bd5e8d
Evanres <evanreswara@gmail.com>
2021-01-10 22:32:39 +0700

    dark mode profile

--- resources/views/Bendaharainti/user/profil.blade.php
+++ resources/views/Bendaharainti/user/profil.blade.php
@@ -1,32 +1,33 @@
 @extends('layout.maininti')
 @section('title')
-Profile
+    Profile
 @endsection()
 @section('container')
 
-<div class="container my-3">
-  <div class="col-md align-content-md-between">
-    <!-- Profile Image -->
-    <div class="card card-primary card-outline">
-      <div class="card-body box-profile">
+    <div class="container " style="margin-top: 125px;">
+        <div class="col-md align-content-md-between">
+            <!-- Profile Image -->
+            <div class="card card-primary card-outline " style="background-color: #273746; ">
+                <div class="card-body box-profile">
 
-        <h3 class="profile-username text-center">{{$user->fullname}}</h3>
+                    <h3 class="profile-username text-center" style="background-color: #273746;">{{ $user->fullname }}</h3>
 
-        <p class="text-muted text-center">{{$user->email}}</p>
+                    <p class="text-muted text-center" style="background-color: #273746;">{{ $user->email }}</p>
 
-        <ul class="list-group list-group-unbordered mb-3">
-          <li class="list-group-item">
-            <b>Nim</b> <a class="float-right">{{$user->nim}}</a>
-          </li>
-          <li class="list-group-item">
-            <b>Divisi</b> <a class="float-right">{{$user->divisi}}</a>
-          </li>
-        </ul>
-      </div>
-      <!-- /.card-body -->
+                    <ul class="list-group list-group-unbordered mb-3 " style="background-color: #273746;">
+                        <li class="list-group-item" style="background-color: #273746;">
+                            <b>Nim</b> <a class="float-right">{{ $user->nim }}</a>
+                        </li>
+                        <li class="list-group-item" style="background-color: #273746;">
+                            <b>Divisi</b> <a class="float-right">{{ $user->divisi }}</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+
+        </div>
     </div>
-    <!-- /.card -->
-
-
-  </div>
-  @endsection
+@endsection
